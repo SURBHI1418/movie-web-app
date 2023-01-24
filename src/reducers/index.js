656d593a43ef3ab5c55e1e6cf653e1ae71ac20fe
Surbhi <surbhilucky05@gmail.com>
2023-01-24 11:11:19 +0530

    use the package of combineReducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,3 +1,4 @@
+import { combineReducers } from "redux";
 import { ADD_MOVIES, ADD_TO_FAVOURITES,REMOVE_FROM_FAVOURITES,SET_SHOW_FAVOURITES } from "../actions";
 
 const intitalMoviesState={
@@ -56,9 +57,13 @@ const initialRootState={
     search: intialSearchState
 
 };
-export default function rootReducer(state= initialRootState,action){
-    return{
-        movies:movies(state.movies,action),
-        search:search(state.search,action) 
-    }
-}
+// export default function rootReducer(state= initialRootState,action){
+//     return{
+//         movies:movies(state.movies,action),
+//         search:search(state.search,action) 
+//     }
+// }
+ export default combineReducers({
+    movies,
+     search
+ })
